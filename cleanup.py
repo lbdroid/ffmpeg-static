1f063d00f45e27fc3adad5120ae7bd728bd1a8f7
Jason Stevens <jay@wizardofthenet.com>
2016-05-12 01:49:49 -0700

    update cleanup script

--- cleanup.py
+++ cleanup.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
 import os
+import argparse
 
-deleteList = ['compile', 'output', 'sourcetar', 'target', 'output.tar.xz']
-#deleteList.append('sourcegit')
-for item in deleteList:
-    os.system('rm -rf ./%s' % item)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Cleanup build folders: compile output target output.tar.xz')
+    parser.add_argument('-a', '--all', dest='all',
+            help='cleanup all (including "sourcegit" and "sourcetar")', action='store_true',
+            default=False)
+    args = parser.parse_args()
+
+    deleteList = ['compile', 'output', 'target', 'output.tar.xz']
+    if args.all is True:
+        deleteList.append('sourcetar')
+        deleteList.append('sourcegit')
+    for item in deleteList:
+        os.system('rm -rf ./%s' % item)
+
 
